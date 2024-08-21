4b223e832750c4cdf6d2c5d06dac4a48d6c2a6d8
olikiverjesus <149892232+olikiverjesus@users.noreply.github.com>
2024-08-21 20:44:20 -0300

    Changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './src/styles/styles.css'; // Importando o CSS global
+import './styles/styles.css'; // Importando o CSS global
 
 ReactDOM.render(
   <React.StrictMode>
