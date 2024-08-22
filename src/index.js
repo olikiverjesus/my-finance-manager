b14993d0979550cf0844b57c1201fa68740220cd
olikiverjesus <149892232+olikiverjesus@users.noreply.github.com>
2024-08-21 21:13:19 -0300

    Changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/styles.css';
+import './styles/styles.css'; // Certifique-se de que o caminho está correto
 
 ReactDOM.render(
   <React.StrictMode>
